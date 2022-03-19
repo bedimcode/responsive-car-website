d695ce21f8309425a8a0ccc64f49408848b04b4b
bedimcode <61248995+bedimcode@users.noreply.github.com>
2022-03-19 09:15:22 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/*=============== SHOW MENU ===============*/
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== POPULAR SWIPER ===============*/
+
+
+/*=============== MIXITUP FILTER FEATURED ===============*/
+
+
+/* Link active featured */ 
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
